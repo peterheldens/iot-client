255cc98aa5700bb55d19089f5d3f74ab17fad275
Peter Heldens <peter.heldens@live.nl>
2020-10-11 20:48:11 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -9,6 +9,32 @@
  */
 //% weight=100 color=#0fbc11 icon=""
 namespace IoT_client {
+        //% block
+    export function register_device () {
+        basic.clearScreen()
+        if (identity < 0) {
+            while (identity < 0) {
+                radio.sendValue("register", 0)
+                led.toggle(2, 2)
+                basic.pause(200)
+            }
+        } else {
+            basic.showString("already registered")
+        }
+        basic.clearScreen()
+        who()
+    }
+    //% block
+    export function unregister_device () {
+        basic.clearScreen()
+        if (identity >= 0) {
+            radio.sendValue("del", control.deviceSerialNumber())
+            led.toggle(2, 2)
+            basic.pause(1000)
+        } else {
+            basic.showString("already deleted")
+        }
+    }
     //% block
     export function showRadioChannel(): number {
         return(101)
@@ -35,5 +61,6 @@ radio.onReceivedValue(function (name, value) {
 radio.setGroup(101)
 radio.setTransmitSerialNumber(true)
 radio.setTransmitPower(7)
-serialRead = ""
-doCommands = false
+let serialRead = ""
+let identity=-1
+let doCommands = false
